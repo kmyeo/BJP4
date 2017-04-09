08dc32fb8f8590ef27d2dd3b73c615f80f400f68
Kyung Min Yeo <kyungmyeo@gmail.com>
2017-04-09 13:29:51 -0400

    Create maxOccurrences.java

---
+++ maxOccurrences.java
@@ -0,0 +1,11 @@
+public int maxOccurrences (List<Integer> list) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int n : list) {
+        if (!map.containsKey(n)) map.put(n, 1);
+        else map.put(n, map.get(n) + 1);
+    }
+    int max = 0;
+    for (int val : map.values())
+        max = Math.max(max, val);
+    return max;
+}
